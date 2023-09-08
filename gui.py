2c791ae6571fb56e2d0780d59dd3e019a074f41e
mmittal2 <mittal.medha@gmail.com>
2023-09-07 23:11:34 -0700

    added cards to gui

--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
 from tkinter import *
 import tkinter.font
-#from PIL import Image
+from PIL import Image, ImageTk
 
 # create window
 root = Tk()
@@ -15,13 +15,22 @@ font_tuple = tkinter.font.Font(family="Comic Sans MS", size=12)
 frame = Frame(root)
 frame.pack()
 # resize the pictures
-"""
 card_back_img = Image.open("back.png")
-card_back_img = card_back_img.resize((50, 100))
-"""
+card_back_img = card_back_img.resize((60, 100))
+card_back_img = ImageTk.PhotoImage(card_back_img)
 # add buttons for each card
-card_1 = Button(frame, image="./AdvancedSoftwareProjects_Project1/back.png")
-# add buttons
+nums = list(range(0, 39))
+x = 0
+y = 0
+buttons = dict()
+for n in nums:
+    buttons[n] = Button(frame, image=card_back_img)
+    buttons[n].grid(row=x, column=y)
+    y += 1
+    if y == 13:
+        y = 0
+        x += 1
+# add game functionality buttons
 start_game_btn = Button(root, text="Start Game", font=font_tuple)
 start_game_btn.pack()
 
